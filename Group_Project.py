e52a3cbf87509019f857682e733f4632bb8f4e38
kirstygranths <106394223+kirstygranths@users.noreply.github.com>
2022-06-21 18:21:59 +0100

    Update Group_Project.py

--- Group_Project.py
+++ Group_Project.py
@@ -1,5 +1,9 @@
 import csv
 import statistics
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def read_data():
@@ -8,12 +12,87 @@ def read_data():
     with open('sales.csv', 'r') as csv_file:
         spreadsheet = csv.DictReader(csv_file)
         for row in spreadsheet:
-            sls_value = row['sales']
-            data.append(sls_value)
+            data.append(row)
+
 
     return data
 
 print(read_data())
-print(max(read_data()))
-print(min(read_data()))
-print(type(read_data()))
+
+def run():
+    data = read_data()
+
+    sales = []
+    for row in data:
+        sale = int(row['sales'])
+        sales.append(sale)
+
+
+    return sales
+
+print(run())
+
+
+total = sum(run())
+max_sales = max(run())
+min_sales = min(run())
+ave_sales = round(statistics.mean(run()),2)
+
+
+print(f'Total sales: {total}')
+print(f'Highest sales: {max_sales}')
+print(f'Lowest sales: {min_sales}')
+print(f'Average sales: {ave_sales}')
+
+
+#CREATE DATAFRAME AND ADD % CHANGE
+df = pd.read_csv("sales.csv")
+
+df['sales_pct_change'] = round(df['sales'].pct_change(),4)
+
+#ADD PROFIT COLUMN AND PROFIT % CHANGE
+df['profit'] = df['sales'] - df['expenditure']
+df['profit_pct_change'] = round(df['profit'].pct_change(),4)
+
+print(df)
+total_profit = sum(df['profit'])
+max_profit = max(df['profit'])
+min_profit = min(df['profit'])
+ave_profit = round(statistics.mean(df['profit']),2)
+
+print(f'Total profit:{total_profit}')
+print(f'Highest profit:{max_profit}')
+print(f'Lowest profit:{min_profit}')
+print(f'Average profit:{ave_profit}')
+
+#FORMATTING DF TO SHOW SLS, PROFIT, EXPENDITURE ONLY
+new_df = df.drop(['year','sales_pct_change','profit_pct_change'],1)
+print(new_df)
+
+#MELTING DATAFRAME TO PREP FOR GRAPH SHOWING SLS, PROFIT AND EXPENDITURE
+df_melt = pd.melt(new_df, id_vars=['month'])
+print(df_melt)
+
+
+#PLOTTING SALES AND PROFIT GRAPH
+sns.set_theme()
+
+sns.relplot(
+    data = df,
+    x="month", y="sales", kind="line")
+
+plt.show()
+
+#sns.relplot(
+    #data = df,
+    #x="month", y="profit", kind="line")
+
+#plt.show()
+
+
+#PLOTTING MELTED DF OF SLS, PROFIT, EXPENDITURE
+sns.relplot(
+    data = df_melt, kind="line",
+    x="month", y="value", hue="variable")
+
+plt.show()
